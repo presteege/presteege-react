5b4fe02c8cf6217b801f68d29d7adedde2f09fcb
art <artgreg@outlook.fr>
2016-03-06 09:23:24 +0100

    - set state before ajax for safari

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,25 +6,54 @@ class Contact extends React.Component{
 
     super(props);
 
-    this.state = { formValidate: false };
+    this.state = { formValidate: false, ajaxStatus: '' };
   }
 
-  handleSubmit(e){
+  handleClick(e){
 
-    console.log(e.currentTarget.id);
+    var oData = new FormData(document.forms.namedItem('contact'));
+    var oReq = new XMLHttpRequest();
 
     this.setState({ formValidate: true });
+
+    oReq.open('POST', 'https://script.google.com/macros/s/AKfycbyAmsqxG4MDWaqRKGU9mYPlY7sHcyLr8cDuBp2v5NTf1b9uo6E/exec', true);
+    oReq.onload = function(oEvent)
+    {
+      if (oReq.status == 200)
+      {
+        var j = JSON.parse(oReq.responseText);
+
+        for(var prop in j){
+
+          if(prop == 'data'){
+
+            var j2 = JSON.parse(j[prop]);
+
+            for(var prop2 in j2){
+
+              console.log(prop2+' '+ j2[prop2]);
+            }
+          }
+        }
+
+      }else {
+
+        this.setState({ ajaxStatus: 'Error ' + oReq.status });
+      }
+    };
+
+    oReq.send(oData);
+
   }
 
   render() {
-
-    console.log(this.state);
 
     if(this.state.formValidate){
 
       return (
         <div className="index">
-          Merci!
+          Merci!<br/>
+          {this.state.ajaxStatus !== '' ? this.state.ajaxStatus : ''}
         </div>
       );
 
@@ -32,12 +61,14 @@ class Contact extends React.Component{
     {
       return (
         <div className="index">
-          <form>
+          <form name="contact" method="post">
            <p><input type="text" name="name" placeholder="Votre nom prénom"/></p>
               <p><input type="email" name="email" placeholder="Votre email"/></p>
-              <p><textarea placeholder="Votre message"></textarea></p>
-              <button type="submit" id="link_send" onClick={this.handleSubmit.bind(this)}>Envoyer</button>
+              <p><textarea placeholder="Votre message" name="message"></textarea></p>
           </form>
+          <p>
+            <span style={{cursor: 'pointer'}}  id="link_send" onClick={this.handleClick.bind(this)}>Envoyer</span>
+          </p>
         </div>
       );
     }
